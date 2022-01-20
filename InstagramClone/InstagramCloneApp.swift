a531f57ee41ab59b99c2e39d854cdda1d3eae1c8
jperez1478 <jessicperez3@gmail.com>
2022-01-20 16:27:04 -0600

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Jessica Perez on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
